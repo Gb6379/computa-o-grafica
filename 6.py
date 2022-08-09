a7a589f0efd1c75af9f376b25bb62f84baa6c02c
Gb6379 <gabriel.am.rosa@gmail.com>
2022-08-09 00:06:29 -0300

    removing unecessary files

--- 6.py
+++
@@ -1,9 +0,0 @@
-print("type a value for vector1 x, y")
-vx = int (input())
-vy = int (input())
-print("type a value for e ")
-e = int(input())
-x = e/vx
-y = e/vy
-print('[',x,']')
-print('[',y,']')
